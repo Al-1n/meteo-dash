c604224b9ca1578cbeb3ad8287124f3c247ea320
Alin Airinei <alin.d33jay@gmail.com>
2025-09-13 00:52:55 +0000

    Fixing chart width in info box

--- pages/3_📈_Time_Series.py
+++ pages/3_📈_Time_Series.py
@@ -228,7 +228,7 @@ with st.expander("See explanation", expanded = True):
                                           yaxis_title='Count',
                                           margin=dict(l=20, r=0, b=20, t = 30),
                                           height = 250,
-                                          #width = 400,
+                                          width = 400,
                                           shapes=[go.layout.Shape(
                                                 type='rect',
                                                 xref='paper',
